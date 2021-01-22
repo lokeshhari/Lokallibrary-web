1979a1e9292ae7f4bd1665f42b0e780c30425a8e
lokeshtom <66105152+lokeshtom@users.noreply.github.com>
2021-01-22 10:19:50 +0530

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -131,7 +131,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS= True
 EMAIL_HOST_USER='lokallibrary@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hari@1999'
+EMAIL_HOST_PASSWORD = 'dqyxdxdvsmjckmkz'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
